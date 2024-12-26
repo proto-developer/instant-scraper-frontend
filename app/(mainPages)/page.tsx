7b5f179b12d4ff08e409fdf2b88e726a678984f6
Babar78 <muhammadbabarashraf8@gmail.com>
2024-12-26 23:49:00 +0500

    feat: basic_layout_setup_complete

---
+++ app/(mainPages)/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center">
+      Home Page <br />
+      <button className="bg-red-300 p-4">Add To Cart</button>
+    </main>
+  );
+}
