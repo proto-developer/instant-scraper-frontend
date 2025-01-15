f814c2edef9883be4b744a8babfbe33376328504
Irtiqa Haider <irtiqahaidermalik@gmail.com>
2025-01-16 01:27:40 +0500

    Added new file

---
+++ components/ui/ProfileMenu.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import LogoutButton from "@/components/ui/LogoutButton";
+
+export default function ProfileMenu() {
+  const [menuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="relative">
+      {/* Profile Photo */}
+      <button
+        className="w-12 md:w-14 h-12 md:h-14 rounded-full bg-gray-300 overflow-hidden border-2 border-white shadow-md"
+        onClick={toggleMenu}
+      >
+        <img
+          src="/Avatar.png" // Replace with your profile image path
+          alt="Profile"
+          className="w-full h-full object-cover"
+        />
+      </button>
+
+      {/* Dropdown Menu */}
+      {menuOpen && (
+        <div className="absolute right-0 mt-2 w-56 bg-[#000] shadow-lg rounded-md z-50">
+          <div className="p-2">
+            <Link
+              href={`/my-account`}
+              className="flex items-center gap-[12px] p-[12px_16px] hover:bg-[#282829] rounded-md"
+            >
+              <Image
+                src="/person.png"
+                alt="My Account"
+                width={20}
+                height={20}
+                className="w-6 md:w-5 lg:w-6"
+              />
+              <p className="text-[16px] md:text-[14px] text-white">My Account</p>
+            </Link>
+
+            <Link
+              href={`/subscription-plan`}
+              className="flex items-center gap-[12px] p-[12px_16px] hover:bg-[#282829] rounded-md"
+            >
+              <Image
+                src="/subscription.png"
+                alt="Subscription"
+                width={20}
+                height={20}
+                className="w-6 md:w-5 lg:w-6"
+              />
+              <p className="text-[16px] md:text-[14px] text-white">Subscription</p>
+            </Link>
+
+            <div className="flex items-center gap-[12px] p-[12px_16px] hover:bg-[#282829] rounded-md cursor-pointer">
+              <LogoutButton />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
